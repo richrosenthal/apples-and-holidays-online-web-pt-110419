a471e4253bbede62621008614be92f6a07b11484
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-05 22:32:46 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,43 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+ holiday_hash[:summer][:fourth_of_july][1]
+ 
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
 end
+ 
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  
+  holiday_hash[:spring].each do |holiday, supplies|
+    supplies << supply
+  end
 end
+  
+
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
-end
+  holiday_hash[season][holiday_name] = supply_array
+ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +65,26 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday_name, supplies|
+    puts "  #{holiday_name.to_s.split("_").collect   {|split_holiday_name| split_holiday_name.capitalize}.join(" ")}: #{supplies.join(", ")}" 
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+   bbq_holiday = []
+  holiday_hash.each do |season, holidays_and_supplies|
+    holidays_and_supplies.each do |holiday, supplies|
+    if supplies.include?("BBQ")
+      bbq_holiday << holiday
+     end
+    end
+  end
+  bbq_holiday
 end
 
 
