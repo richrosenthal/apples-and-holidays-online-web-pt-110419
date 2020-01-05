10cc5b2130532f0b303095e0efc36f50d4bc024a
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-05 22:18:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each |holiday_name, supplies|
+    puts ""
     
+  end
     
  end 
 end
