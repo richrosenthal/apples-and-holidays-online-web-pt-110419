f8986db845cdb193a930aead76d1725f73fa9a7d
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-05 22:27:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each |holiday_name, supplies|
-    puts "  #{holiday_name.to_s.split(pattern "_").collect {|split_holiday_name| }"
+    puts "  #{holiday_name.to_s.split(pattern "_").collect {|split_holiday_name| split_holiday_name.capitalize}.join(" 
     
   end
     
